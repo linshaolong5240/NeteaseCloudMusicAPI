0ff49e629f1f3cf0c320cd6caf8a9aadd8070ec9
linshaolong5240 <634205468@qq.com>
2022-02-02 17:34:40 +0800

    add demo project

---
+++ NeteaseCloudMusicAPIDemo/Shared/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by teenloong on 2022/2/2.
+//
+
+import SwiftUI
+import NeteaseCloudMusicAPI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NeteaseCloudMusicAPIDemo/Shared/NeteaseCloudMusicAPIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeteaseCloudMusicAPIDemoApp.swift
+//  Shared
+//
+//  Created by teenloong on 2022/2/2.
+//
+
+import SwiftUI
+
+@main
+struct NeteaseCloudMusicAPIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
