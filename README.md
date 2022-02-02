ef65a4d3e2547d0c28c166c74543a246e6b9c5ae
linshaolong5240 <634205468@qq.com>
2022-02-02 17:12:59 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# NeteaseCloudMusicAPI
+
+A description of this package.
---
+++ Tests/NeteaseCloudMusicAPITests/NeteaseCloudMusicAPITests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import NeteaseCloudMusicAPI
+
+final class NeteaseCloudMusicAPITests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+//        XCTAssertEqual(NeteaseCloudMusicAPI().text, "Hello, World!")
+    }
+}
