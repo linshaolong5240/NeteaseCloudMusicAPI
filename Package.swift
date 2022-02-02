96df9c76aaba70f40a1f69b8191ad68335b67db3
linshaolong5240 <634205468@qq.com>
2022-02-02 17:17:18 +0800

    support platforms

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "NeteaseCloudMusicAPI",
-    platforms: [.iOS(.v10), .macOS(.v11)],
+    platforms: [.iOS(.v8), .macOS(.v10_12)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
